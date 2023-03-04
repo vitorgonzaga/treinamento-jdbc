446aa707f96ccc41c025fe45ac5642bd72a6435e
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-03-04 13:42:21 -0300

    setting a connection pool with ComboPooledDataSource and DataSource

--- src/main/java/org/example/ConnectionFactory.java
+++ src/main/java/org/example/ConnectionFactory.java
@@ -1,15 +1,38 @@
 package org.example;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
+
+    public DataSource dataSource;
+
+    public ConnectionFactory() {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost:3306/lojavirtual");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("root");
+        // configurações adicionais
+        comboPooledDataSource.setMaxPoolSize(15);
+
+        this.dataSource = comboPooledDataSource;
+    }
+
     public Connection recuperaConexao() throws SQLException {
+/*
+        // utilização sem a configuração de um Pool de conexões
         return DriverManager
                 .getConnection(
                         "jdbc:mysql://localhost:3306/lojavirtual",
                         "root",
                         "root");
+*/
+
+        return this.dataSource.getConnection();
+
     }
 }
